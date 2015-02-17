416a321d27405514deabea6d21b9e351840f2d13
Administrator <admin@local.host>
2015-02-17 09:51:08 -0500

    trying to use what pretender is returning. not working

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
+        var users = undefined;
+        users = this.store.find('user');
+        console.log('got this :', JSON.stringify(users) );
+        //return { users : users };
         return { users : [ {id:'id1', mydata:'data1' } ] };
     },
 });
